87fb9d6be7e4fc02e7fd7f62e8e0cadf69fd7401
Unknown <treshone994s@gmail.com>
2019-05-17 22:03:15 +0500

    added Category and Product models and image uploader for products

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+  belongs_to :category
+  mount_uploader :image, ImageUploader
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+end
