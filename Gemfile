105f1e85d4bb8daaf6723ad0f3d251c3f078ca15
Unknown <treshone994s@gmail.com>
2019-05-18 10:51:53 +0500

    added admin authentication with trestle-auth

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'carrierwave', '~> 1.0'
 gem 'trestle'
+gem 'trestle-auth'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_164622) do
+ActiveRecord::Schema.define(version: 2019_05_18_054650) do
+
+  create_table "administrators", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "remember_token"
+    t.datetime "remember_token_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
