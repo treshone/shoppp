444ed88f9ba687514771a3ea5315f8291336b681
Unknown <treshone994s@gmail.com>
2019-06-11 21:20:26 +0500

    updated  validations error messages

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,10 @@ class Order < ApplicationRecord
   belongs_to :status
   has_many :order_items, dependent: :destroy
 
-  validates :name, presence: true
-  validates :phone, presence: true, numericality: { only_integer: true }, length: { is: 11 }
-  validates :email, presence: true
-  validates :address, presence: true
+  validates :name, presence: { message: 'должно быть заполнено' }
+  validates :phone, presence: { message: 'должен быть заполнен' }, 
+                    numericality: { only_integer: true, message: 'должен состоять из цифр' },
+                    length: { is: 11, message: 'должен состоять из 11 знаков' }
+  validates :email, presence: { message: 'должен быть заполнен' }
+  validates :address, presence: { message: 'должен быть заполнен' }
 end
